9ab3b4a9183548fc642ce0bb8a6628075d518d02
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-02-03 22:17:03 +0630

    npm speedup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import { ToastProvider } from '../context/ToastContext';
 import ChatWidget from '../components/social/ChatWidget'; // NEW: Import Chat
 import '../styles/globals.css';
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
