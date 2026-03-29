6547e4ffd62008520e27d3ea438294775a199d2e
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-03-29 11:26:45 +0630

    doc V2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="dark">
       <Head>
+        <meta charSet="utf-8" />
         <meta name="application-name" content="Suropara" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
@@ -11,15 +12,20 @@ export default function Document() {
         <meta name="description" content="Play the #1 Slot Game in Myanmar. 3D Graphics, Instant Withdrawals, and Huge Jackpots." />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#050505" />
 
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="/favicon.ico" />
         
-        {/* Apple Touch Icon (You need to add this file to public/icons) */}
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        {/* Apple Touch Icon (Mapped to the standard 192x192 maskable icon) */}
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        
+        {/* Preconnect to fonts for performance to prevent layout shift */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800;900&family=JetBrains+Mono:wght@400;700;900&display=swap" rel="stylesheet" />
       </Head>
-      <body>
+      <body className="bg-[#050505] text-gray-100 antialiased overflow-x-hidden selection:bg-cyan-500 selection:text-black">
         <Main />
         <NextScript />
       </body>
